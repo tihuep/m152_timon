6616b13a8a0bee9bf955081fe22be2c099a26b24
Timon Hüppi <timon.hueppi@gmail.com>
2021-04-07 14:16:07 +0200

    initial commit adds base website

---
+++ README.md
@@ -0,0 +1,23 @@
+# m152_thstudios
+
+ThStudios is an imagimary Web-Development Studio.
+
+## Styleguide
+
+Following is the styleguide of this website.
+
+### Colors
+
+https://www.webmart.de/web/harmonische-farben#ff7b00:t:s:g:d
+
+### Fonts
+
+Trebuchet MS
+
+### Inspiratoin
+
+https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+
+https://bootstrapious.com/p/bootstrap-vertical-navbar
+
+https://stackoverflow.com/questions/18540474/how-to-rotate-image-when-scrolling-using-css-transform/18540800 - http://jsfiddle.net/g3k6h/5/
---
+++ js/main.js
@@ -0,0 +1,32 @@
+AOS.init();
+
+var rotation = 0;
+var prevRotation = 0;
+document.addEventListener("scroll", function(oEvent) {
+    var diff = window.scrollY - prevRotation;
+    prevRotation = window.scrollY;
+    rotation += diff;
+    var rotationStr = "rotate(" + rotation/5 + "deg)";
+    document.querySelectorAll(".profilePicture").forEach(function(item){
+        item.style = "-webkit-transform:" + rotationStr + 
+                    ";-moz-transform: " + rotationStr + 
+                    "; transform:" + rotationStr;
+    });
+    console.log(rotationStr);
+});
+
+document.querySelector("#sidebarCollapse").addEventListener("click", function(oEvent) {
+    document.querySelector("#sidebar").classList.toggle("active");
+    document.querySelector("#content").classList.toggle("active");
+
+    if (document.querySelector("#sidebarCollapse").parentElement.id == "content"){
+        document.querySelector("#profile").before(document.querySelector("#sidebarCollapse"));
+        document.querySelector("#sidebarCollapse").style.marginTop = "2vh";
+        document.querySelector("#sidebarCollapse").style.parginLeft = "2vh";
+    }else if (document.querySelector("#sidebarCollapse").parentElement.id == "sidebar"){
+        document.querySelector("#hero").before(document.querySelector("#sidebarCollapse"));
+        document.querySelector("#sidebarCollapse").style.marginTop = "0";
+        document.querySelector("#sidebarCollapse").style.marginLeft = "0";
+    }
+});
+
